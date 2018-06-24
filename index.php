26ac0d21809b3376b7fb7e61ee9b4012a7647ea0
osama elyan <40536843+Osamaelkalkily@users.noreply.github.com>
2018-06-24 12:01:26 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Add this css File in head tag-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet" media="all">
+    <!-- _______________________________________________________________________Script.js-->
+    <script type="text/javascript" src="js/script.js" ></script>
+    <!--________________________________________________________________________Jquery Link -->
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!--________________________________________________________________________Bootstrap Link -->   
+    <script src="js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <!-- _______________________________________________________________________bootStrap Style-->
+    <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+    <!--________________________________________________________________________Style.css-->
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+    </head>
+    <body>
+       <div class="container">
+            <img src="images/images/log_03.jpg" class="img-responsive logo"> 
+        </div>
+        
+        <nav  class="navbar navbar-default" style=" margin-bottom:0px !important; border:none !important;">
+          <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span class="sr-only"><a href="#"></a></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav container">
+                    <li><a href="index.php">HOME        </a></li>
+                    <li><a href="index.php">SALE        </a></li>
+                    <li><a href="index.php">HADNBAGS    </a></li>
+                    <li><a href="index.php">WALLETS     </a></li>
+                    <li><a href="index.php">ACCESSORIES </a></li>
+                    <li><a href="index.php">MEN'S STORE </a></li>
+                    <li><a href="index.php">SHOES       </a></li>
+                    <li><a href="index.php">VINTAGE     </a></li>
+                    <li><a href="aboutus.php">SERVICE   </a></li> 
+                    <li><a href="aboutus.php">CONTACT US</a></li> 
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+        <!--___________________________________________________________________End Of Nav Bar-->  
+        <div class="container-fluid">
+        <div class="row">
+        <div>
+            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+                
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <img src="images/repea.jpg" class="slider_pic">
+                        <div class="carousel-caption" style="padding-bottom: 8% !important;">
+                            <img src="images/t-shirt8_03_03.png" class="col-md-4 col-xs-9 col-sm-6  img-responsive t-shirt" style="margin-right: 60px;">
+                            <h3 style="text-align:start; font-size:36px;">Cowhide <br> Standard Crew</h3>
+                            <p style="text-align:start; width:69%;">White coloured, short-sleeved, printed T-shirt for men by Levi's. This crew-neck T-shirt is made of organic cotton and comes in a regular fit.</p>
+                            <a type="button" class="btn btn-success">SHOP NOW</a>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="images/repea.jpg" class="slider_pic">
+                        <div class="carousel-caption" style="padding-bottom: 8% !important;">
+                            <img src="images/t-shirt8_03_03.png" class="col-md-4 col-xs-9 col-sm-6  img-responsive t-shirt" style="margin-right: 60px;">
+                            <h3 style="text-align:start; font-size:36px;">Cowhide <br> Standard Crew</h3>
+                            <p style="text-align:start; width:69%;">White coloured, short-sleeved, printed T-shirt for men by Levi's. This crew-neck T-shirt is made of organic cotton and comes in a regular fit.</p>
+                            <a type="button" class="btn btn-success">SHOP NOW</a>
+                        </div>
+                    </div>
+                </div>
+                <a href="#carousel-example-generic" data-slide="prev"></a>
+                <a href="#carousel-example-generic" data-slide="next"></a>       
+            </div>
+        </div>
+    </div>
+</div>     
+
+        
+        <style>
+            
+.feature{       
+                margin-top:30px;
+                background-color:#42c0b8;
+                width:100%;
+                height:65px;
+                color:#fff;
+                text-align:center;
+            }
+        </style>
+        <div class="feature container">
+            <h3 class="col-xs-12 col-md-5">FEATURED PRODUCTS</h3>
+        </div>
+<!-- cart for buy -->
+<div class="container cart">
+    <div class="new col-md-4 col-xs-12">
+        <img src="images/shoes.png.png" alt="Red Shoes" class="prod_pic img-responsive">
+        <h3 class="prod_name">Branded Shoe</h3>
+        <p class="prod_price">$300</p>
+       <a class="btn btn-default prod_button" href="#" role="button">BUY NOW</a> 
+    </div>
+    <div class="new col-md-4 col-xs-12">
+        <img src="images/new-t--shirt_06.jpg" alt="Red Shoes" class="prod_pic img-responsive">
+        <h3 class="prod_name">Branded Shoe</h3>
+        <p class="prod_price">$150</p>
+       <a class="btn btn-default prod_button" href="#" role="button">BUY NOW</a> 
+    </div>
+    <div class="new col-md-4 col-xs-12">
+        <img src="images/new-t-shirt_03.jpg" alt="Red Shoes" class="prod_pic img-responsive">
+        <h3 class="prod_name">Branded Shoe</h3>
+        <p class="prod_price">$300</p>
+       <a class="btn btn-default prod_button" href="#" role="button">BUY NOW</a> 
+    </div>
+    <div class="new col-md-4 col-xs-12">
+        <img src="images/walet_11.jpg" alt="Red Shoes" class="prod_pic img-responsive">
+        <h3 class="prod_name">Branded Shoe</h3>
+        <p class="prod_price">$300</p>
+       <a class="btn btn-default prod_button" href="#" role="button">BUY NOW</a> 
+    </div>
+    <div class="new col-md-4 col-xs-12">
+        <img src="images/shoes.png.png" alt="Red Shoes" class="prod_pic img-responsive">
+        <h3 class="prod_name">Branded Shoe</h3>
+        <p class="prod_price">$300</p>
+       <a class="btn btn-default prod_button" href="#" role="button">BUY NOW</a> 
+    </div>
+    <div class="new col-md-4 col-xs-12">
+        <img src="images/walet_11.jpg" alt="Red Shoes" class="prod_pic img-responsive">
+        <h3 class="prod_name">Branded Shoe</h3>
+        <p class="prod_price">$300</p>
+       <a class="btn btn-default prod_button" href="#" role="button">BUY NOW</a> 
+    </div>
+    
+    
+</div>
+               
+<footer class="foot">
+    <div class="container-fluid first">
+        <div class="col-md-3 col-xs-3 col1">
+            <h3>FEATURE SALE</h3>
+                <div class="links">
+                    <div>
+                        <a href="#">Alexis Hudson</a>
+                    </div>
+                    <div>
+                        <a href="#">American Apparel</a>
+                    </div>
+                    <div>
+                        <a href="#">Ben Sherman</a>
+                    </div>
+                    <div>
+                        <a href="#">Big Buddha</a>
+                    </div>
+                    <div>
+                        <a href="#">Chanel</a>
+                    </div>
+                    <div>
+                        <a href="#">Christian Audigier</a>
+                    </div>
+                    <div>
+                        <a href="#">Coach</a>
+                    </div>
+                    <div>
+                        <a href="#">Cole Haan</a>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 col-xs-3 col2">
+            <h3>MENS STORE</h3>
+                <div class="links">
+                    <div>
+                        <a href="#">Alexis Hudson</a>
+                    </div>
+                    <div>
+                        <a href="#">American Apparel</a>
+                    </div>
+                    <div>
+                        <a href="#">Ben Sherman</a>
+                    </div>
+                    <div>
+                        <a href="#">Big Buddha</a>
+                    </div>
+                    <div>
+                        <a href="#">Chanel</a>
+                    </div>
+                    <div>
+                        <a href="#">Christian Audigier</a>
+                    </div>
+                    <div>
+                        <a href="#">Coach</a>
+                    </div>
+                    <div>
+                        <a href="#">Cole Haan</a>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 col-xs-3 col3">
+            <h3>WOMEN STORE</h3>
+                <div class="links">
+                    <div>
+                        <a href="#">Alexis Hudson</a>
+                    </div>
+                    <div>
+                        <a href="#">American Apparel</a>
+                    </div>
+                    <div>
+                        <a href="#">Ben Sherman</a>
+                    </div>
+                    <div>
+                        <a href="#">Big Buddha</a>
+                    </div>
+                    <div>
+                        <a href="#">Chanel</a>
+                    </div>
+                    <div>
+                        <a href="#">Christian Audigier</a>
+                    </div>
+                    <div>
+                        <a href="#">Coach</a>
+                    </div>
+                    <div>
+                        <a href="#">Cole Haan</a>
+                    </div>
+                </div>
+        </div>
+        <div class="col-md-3 col-xs-3 col4">
+            <h3>QUICK LINKS</h3>
+                <div class="links">
+                    <div>
+                        <a href="#">Alexis Hudson</a>
+                    </div>
+                    <div>
+                        <a href="#">American Apparel</a>
+                    </div>
+                    <div>
+                        <a href="#">Ben Sherman</a>
+                    </div>
+                    <div>
+                        <a href="#">Big Buddha</a>
+                    </div>
+                    <div>
+                        <a href="#">Chanel</a>
+                    </div>
+                    <div>
+                        <a href="#">Christian Audigier</a>
+                    </div>
+                    <div>
+                        <a href="#">Coach</a>
+                    </div>
+                    <div>
+                        <a href="#">Cole Haan</a>
+                    </div>
+            </div>
+        </div>
+    </div>
+            <div class="second">
+              <p>Copyright © <?php echo date('Y'); ?> Aditii Team </p>  
+            </div>
+</footer>
+            
+    </body>
+</html>
